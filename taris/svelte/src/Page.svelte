30f3131ce4e606420a78f1dd9c06aa14d4f16735
pkarasev <p.karasev@psw.ru>
2023-06-28 14:07:13 +0300

    upd

--- taris/svelte/src/Page.svelte
+++ taris/svelte/src/Page.svelte
@@ -5,8 +5,9 @@
 
     let list = [
         {
-            'class': 'o',
-            'text': `<div>Это сразу первая страница. Создать</div>
+            id: 1123213,
+            class1: 'o',
+            text: `<div>Это сразу первая страница. Создать</div>
         <ul>
             <li>Проект</li>
             <li>Файл</li>
@@ -14,25 +15,40 @@
         </ul>`
         },
         {
-            'class': 'o',
-            'text': `<p>Еще одна позиция</p>
+            id: 2234234,
+            class1: 'o',
+            text: `<p>Еще одна позиция</p>
         <p>Еще одна позиция</p>`
         },
         {
-            'class': 'o file',
-            'text': `<a href="./name">name</a>`
-        }
+            id: 334534,
+            class1: 'o file',
+            text: `<a href="./334534">name</a>`
+        },
+        {
+            id: 2234234,
+            class1: 'o',
+            text: `Являясь всего лишь частью общей картины, тщательные исследования конкурентов объединены в целые кластеры себе подобных. Повседневная практика показывает, что постоянный количественный рост и сфера нашей активности предполагает независимые способы реализации форм воздействия. `
+        },
+        {
+            id: 2234234,
+            class1: 'o',
+            text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nemo beatae ratione maiores quo. Odio labore ducimus ipsa adipisci maxime eligendi minus libero cumque veniam maiores. Atque nulla quidem perspiciatis. `
+        },
     ]
 
 </script>
+
+
 
 <p><a href="/">Выйти</a></p>
 
 <div class="list">
     
-    {#each list as o }
-        <div class={o.class}>
-            {@html o.text}
+    {#each list as {id, class1, text}, k }
+        <div class={class1}>
+            <a href="#{id+k}" class="id">#{id+k}</a>
+            {@html text}
         </div>
     {/each}
     
@@ -41,5 +57,6 @@
 
 <style>
     .list   { display: flex; flex-direction: column; gap: 2em; }
-    .o      { border-left: solid 3px #444; padding-left: 1ch; }
+    .o      { border-left: solid 3px #444; padding-left: 1ch; max-width: 700px; }
+    .o .id  { display: block; font-size: 10px; color: #444; }
 </style>
