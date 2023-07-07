a85670b65e7e6f598ec5d8c6c2ac5412bb728019
pkarasev <p.karasev@psw.ru>
2023-07-07 15:50:47 +0300

    add main.tpl.php

--- taris/tpl/main.tpl.php
+++ taris/tpl/main.tpl.php
@@ -1,17 +1,50 @@
 <?php
 load::$layout   =   'default.tpl.php';
-load::$title    =   'Site YYYY';
+load::$title    =   'Tariz';
+
+# стартовый файл
+#
+if ( url::$path == '/' )    url::redir('/r/start');
+#
+function getRow($id)
+{
+    $row   =   file_get_contents(__DIR__. "/res/$id.json" );
+    $row   =   json_decode($row, 1);
+
+    return $row;
+}
+
+$row = getRow(url::$level[1]);
+
+// load::vdd($row, 0);
+
 ?>
 
+<div class="flex1">
+    <div class="inc">
+        inc
+    </div>
+    <div class="list">
 
-<h1>Php Hot Rod</h1>
+        <?
+        
+        foreach( $row['rows'] as $id )
+        {
+            list( 'name'=>$name, 'type'=>$type )  =  getRow($id);
+            
+            ?>
+            <div>
+                <div class="id"><?= $type=='file'?  '<a href="./' .$id. '">' .$id. '</a>' :  $id ?></div>
+                
+                <span class="message"><?= $name ?></span>
+            </div>
+            <?
+        }
+        ?>
 
-<div>SiteYYYY main page</div>
+    </div>
+</div>
 
-<?
-echo array(
-    url::$path  => '<p><a href="/next">Next Page</a></p>',
-    '/next'     => '<p><a href="/">Back</a></p>',
-)[ url::$path ]
-?>
+
+
 
