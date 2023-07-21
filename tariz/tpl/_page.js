d5e9b897ffeb3752a0238a4480d464d860be15ca
pkarasev <p.karasev@psw.ru>
2023-07-21 10:12:30 +0300

    add ace for form tree

---
+++ tariz/tpl/_page.js
@@ -0,0 +1,175 @@
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    
+    ace_init(".ace")
+    
+});
+
+
+document.addEventListener('keydown', function(e) {
+    
+    if (e.code == 'KeyS' && (e.ctrlKey || e.metaKey))
+    {
+        e.preventDefault()
+        $('#btn-save').click()
+    }
+});
+
+
+
+
+function ace_init(selector)
+{
+    ace1    =  [];
+    sess1   =  [];
+    
+    $(selector).each(function(i, t) {
+        
+        var aceid   =   'ace' + i;
+        var $t      =   $(t);
+        $t.hide();
+        $t.after('<div id="' + aceid + '"></div>');
+        
+        ace1[i]   =   ace.edit(aceid, {
+            mode: t.dataset.mode,
+            minLines: 4,
+            maxLines: 1111,
+            wrap: true,
+            // граница печати
+            showPrintMargin: false,
+            // нумерация строк
+            showGutter: true,
+            // тема раскраски кода
+            theme: (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ?  'ace/theme/tomorrow_night':  '')      
+        });
+        ace1[i].focus();
+        // авторесайз
+        ace1[i].setAutoScrollEditorIntoView(true);
+        
+        sess1[i] = ace1[i].getSession();
+        sess1[i].setValue( $(t).val() );
+        // сохранить обратно в textarea
+        sess1[i].on('change', function(){
+            $(t).val(sess1[i].getValue());
+        });
+        
+        
+        /* отключить анотации */
+        sess1[i].on('changeAnnotation', function(){
+            
+            //console.log(sess1[i].$annotations);
+            
+            sess1[i].$annotations  =   sess1[i].$annotations.filter(function(annotation){
+                return !(
+                        /doctype first\. Expected/.test(annotation.text)
+                    ||  /Unexpected End of file\. Expected/.test(annotation.text)
+                    //||    /voodoo mode/.test(annotation.text)
+                    ||  /Trailing solidus not/.test(annotation.text)
+                    ||  /Unexpected end tag/.test(annotation.text)
+                )
+            });
+            
+            ace1[i].$onChangeAnnotation();
+        });
+        
+        
+    })
+    
+    //console.log(t);
+}
+
+
+
+
+
+
+function copyToClipboard(str)
+{
+    var area = document.createElement('textarea');
+    document.body.appendChild(area);  
+    area.value = str;
+    area.select();
+    document.execCommand("copy");
+    document.body.removeChild(area);  
+}
+
+
+
+
+
+
+
+/* загрузка файлов из буфера обмена */
+
+
+document.onpaste = function (event)
+{
+    var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+    
+    for (var index in items)
+    {
+        if (items[index].kind !== 'file' && items[index].type != 'image/png')   continue;
+        
+        let blob        =   items[index].getAsFile();
+        let datetime    =   new Date().toISOString().substring(0,19).replace(/[^\d]/g, '-');
+        
+        let filename    =   'image_' + datetime + '.png'
+        let formData    =   new FormData();
+            formData.set('attachActionUpload', 1);
+            formData.append('f[]', blob, filename);
+            
+        //console.log(formData);
+        //console.log(blob);
+        attach.submit(formData);
+        
+        ace1[0].focus();
+        ace1[0].execCommand("paste", '<p><img src="' + window.location.pathname + '/' + filename + '" /></p>')
+    }
+};
+
+
+
+
+/* приложения */
+
+attach  =   {};
+
+attach.form     =   function(form)
+{
+    let formData    =   new FormData(form);
+        formData.set('attachActionUpload', 1);
+        
+    this.submit(formData)
+}
+
+
+attach.submit   =   async function(formData)
+{
+    let btn         =   $('#upload-bnt');
+        btn.addClass('active');
+        
+    let response    =   await fetch(window.location.href, {method: 'POST', body: formData} );
+    //let result      =   await response.json();
+    let result      =   await response.text();
+    
+    btn.removeClass('active');
+    //console.log(result);
+    
+    
+    $('#files').html(result);
+}
+
+
+attach.delete   =   function(t)
+{
+    let formData    =   new FormData();
+        formData.set('attachActionDelete', 1);
+        formData.set('name', $(t).closest('.descr').children('.name').text() );
+    
+    attach.submit(formData);
+    //let response    =   await fetch(window.location.href, {method: 'POST', body: formData} );
+    //let result      =   await response.text();
+    
+    //console.log(result);
+}
+
--- tariz/tpl/main.tpl.php
+++ tariz/tpl/main.tpl.php
@@ -47,8 +47,8 @@ $project->actionCansel();
     </div>
     
     <div class="opt">
-        <a href="<?= url::$dir[0] ?>" class="<?= !isset(url::$level[1])? 'active': '' ?>">Обзор</a>
-        <a href="<?= url::$dir[0]. '/tree' ?>" class="<?= @url::$level[1]=='tree'? 'active': '' ?>">Проект</a>
+        <a href="<?= url::$dir[0] ?>" class="<?= !isset(url::$level[1])? 'active': '' ?>">Проект</a>
+        <a href="<?= url::$dir[0]. '/tree' ?>" class="<?= @url::$level[1]=='tree'? 'active': '' ?>">Дерево</a>
         <?= $start == $proId  && isset($proBc[1])  ? '<a href="' .url::$dir[0].     '?actionProjectCansel">- Проект</a>' : '' ?>
         <?= $start != $proId && !isset(url::$level[1]) ? '<a href="' .url::$dir[0]. '?actionProjectCreate">+ Проект</a>' : '' ?>
         <a href="<?= url::$dir[0]. '/rows' ?>" class="<?= @url::$level[1]=='rows'? 'active': '' ?>">Записи</a>
@@ -81,11 +81,22 @@ if ( !isset(url::$level[1]) )
 elseif ( url::$level[1] == 'tree' )
 {
     ?>
-    <form method="post">
-        <textarea name="tree" style="width: 100%; height: 20em; padding: 5px;"><?= trim($project->getTextTree( $proId )) ?></textarea>
-        <p><button>Обновить</button></p>
+    <form class="tree" method="post">
+        <textarea class="ace" name="tree" style="width: 100%; height: 20em; padding: 5px;"><?= trim($project->getTextTree( $proId )) ?></textarea>
+        <button class="save">Обновить</button>
     </form>
+
     <?
+    // echo '<script src="' .load::makefile('/t/ace/emmet.js', 'inc/ace/emmet.js', true, false). '"></script>'. "\n";
+    echo '<script src="' .load::makefile('/t/ace/ace.js', 'inc/ace/ace.js', true, false). '"></script>'. "\n";
+    // echo '<script src="' .load::makefile('/t/ace/mode-html.js', 'inc/ace/mode-html.js', true, false). '"></script>'. "\n";
+    // echo '<script src="' .load::makefile('/t/ace/mode-css.js', 'inc/ace/mode-css.js', true, false). '"></script>'. "\n";
+    // echo '<script src="' .load::makefile('/t/ace/worker-html.js', 'inc/ace/worker-html.js', true, false). '"></script>'. "\n";
+    // echo '<script src="' .load::makefile('/t/ace/worker-css.js', 'inc/ace/worker-css.js', true, false). '"></script>'. "\n";
+    // echo '<script src="' .load::makefile('/t/ace/ext-emmet.js', 'inc/ace/ext-emmet.js', true, false). '"></script>'. "\n";
+    // echo '<script src="' .load::makefile('/t/ace/theme-tomorrow_night.min.js', 'inc/ace/theme-tomorrow_night.min.js', true, false). '"></script>'. "\n";
+    echo '<script src="' .load::makefile('/t/_page.js', '_page.js'). '"></script>' . "\n";
+
     
 }
 ?>
