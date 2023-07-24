102a6fbe73557a242d2cc269721d556f35bdff1b
pkarasev <p.karasev@psw.ru>
2023-07-24 15:58:43 +0300

    upd

--- tariz/tpl/main.tpl.php
+++ tariz/tpl/main.tpl.php
@@ -8,12 +8,41 @@ user::actionLoginSend();
 ?>
 
 <div class="main top">
-    <form class="login" method="post">
-        <?= ftoken::input() ?>
-        <input class="email" type="email" name="email" placeholder="Емеил для входа" required="true" />
-        <button class="send">Войти</button>
-    </form>
-    
     <img class="logo" src="/i/TZ.svg" />
+    
+    <div>
+
+    <form class="login" method="post">
+            <?= ftoken::input() ?>
+            <input class="email" type="email" name="email" placeholder="Емеил для входа" required="true" />
+            <button class="send">Войти</button>
+        </form>
+
+        <br>
+        <br>
+
+        <form class="login" method="post">
+            <?= ftoken::input() ?>
+            <div>
+                <div>Код из письма</div>
+            </div>
+            <input class="code" name="code" placeholder="XXXX" required="true" />
+        </form>
+        
+        <br>
+        <br>
+
+        <form class="login" method="post">
+            <?= ftoken::input() ?>
+            <div>
+                <div>Код из письма</div>
+                <div class="err">Плохой код...</div>
+            </div>
+            <input class="code" name="code" placeholder="XXXX" required="true" />
+        </form>
+        
+        
+
+    </div>
     
 </div>
