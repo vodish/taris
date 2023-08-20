f5b12cfa7534a39673de745db354b79c6a94956b
vodish <vodish@yandex.ru>
2023-08-20 22:01:13 +0300

    upd

--- tariz/svelte/src/ui/Pack.svelte
+++ tariz/svelte/src/ui/Pack.svelte
@@ -1,25 +1,25 @@
 <script>
 // @ts-nocheck
-import { url, href }   from "../state/url";
-import PackLine     from "./PackLine.svelte";
-import PackView     from "./PackView.svelte";
-import PackTree     from "./PackTree.svelte";
-import PackAccess   from "./PackAccess.svelte";
-
+import { url, href }  from "../state/url";
+import PackLine       from "./PackLine.svelte";
+import PackView       from "./PackView.svelte";
+import PackTree       from "./PackTree.svelte";
+import PackAccess     from "./PackAccess.svelte";
 
 </script>
 
 <hr>
+
 <h4>
-    Pack: 
-    <button on:click={()=> href("/1") }>View</button>
-    <button on:click={()=> href("/1/line") }>Line</button>
-    <button on:click={()=> href("/1/tree") }>Tree</button>
-    <button on:click={()=> href("/1/access") }>Access</button>
+  Pack: 
+  <button on:click={()=> href("/1") }>View</button>
+  <button on:click={()=> href("/1/line") }>Line</button>
+  <button on:click={()=> href("/1/tree") }>Tree</button>
+  <button on:click={()=> href("/1/access") }>Access</button>
 </h4>
 
-{#if !$url.level[1]} <PackView />
-{:else if $url.level[1] == "line" } <PackLine />
-{:else if $url.level[1] == "tree" } <PackTree />
-{:else if $url.level[1] == "access" } <PackAccess />
+{#if $url.level[1] == undefined     }   <PackView />
+{:else if $url.level[1] == "line"   }   <PackLine />
+{:else if $url.level[1] == "tree"   }   <PackTree />
+{:else if $url.level[1] == "access" }   <PackAccess />
 {/if}
