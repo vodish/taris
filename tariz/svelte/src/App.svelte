7679b2092b772837c83a76613eae7389d5c0e361
vodish <vodish@yandex.ru>
2023-08-20 21:50:04 +0300

    upd

--- tariz/svelte/src/App.svelte
+++ tariz/svelte/src/App.svelte
@@ -3,12 +3,11 @@
   import { url, href } from "./state/url";
   import Main from "./ui/Main.svelte";
   import Pack from "./ui/Pack.svelte";
-
 </script>
 
 <h3>Default: 
   <button on:click={()=> href("/")  }>Main</button>
-  <button on:click={()=> href("/1") }>Pack</button>
+  <button on:click={(e)=> href("/1") }>Pack</button>
 </h3>
 
 {#if $url.path == '/' }  <Main />
