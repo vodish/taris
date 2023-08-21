54d24eecfa2bccfe56c18cae9dab9322f01391b5
vodish <vodish@yandex.ru>
2023-08-21 20:52:48 +0300

    upd

--- tariz/svelte/src/ui/PackTree.svelte
+++ tariz/svelte/src/ui/PackTree.svelte
@@ -5,15 +5,15 @@ import { tree_content, ace_option } from '../state/pack';
 import * as ace from "ace-builds";
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/ext-emmet";
-import "emmet-core"
+import "emmet-core";
 
 
 onMount(() => {
-    const editor = ace.edit('ace9');
-    editor.session.setMode('ace/mode/html');
-    editor.setOptions(ace_option);
-    editor.setValue($tree_content, 1);
-    editor.on('change', () => $tree_content = editor.getValue());
+    const editor = ace.edit('ace9')
+    editor.session.setMode('ace/mode/html')
+    editor.setOptions(ace_option)
+    editor.setValue($tree_content, 1)
+    editor.on('change', () => $tree_content = editor.getValue())
 });
 </script>
 
