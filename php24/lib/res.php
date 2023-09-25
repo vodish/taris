d9744f8e1174506f6032385c4333d8abf3bc83be
pkarasev <p.karasev@psw.ru>
2023-09-25 14:58:16 +0300

    upd

--- php24/lib/res.php
+++ php24/lib/res.php
@@ -2,7 +2,16 @@
 class res
 {
     static $render;
+    
 
+    static function var($var, $val='')
+    {
+        if ( ! in_array($var, req::$wait)  )    return;
+
+        $i  =   array_search($var, req::$wait);
+        
+        req::$wait[ $var ]  =   $val;
+    }
 
 
 
@@ -10,9 +19,10 @@ class res
     {
         if ( self::$render != 'html' )  return;
         
+        res::var('userList', 'sdvsdvsv');
+
         $vars   =   "";
         foreach( req::$wait as $k => $v )    $vars  .=  '<div id="' .$k. '">' .$v. '</div>'. "\n";
-        
         
         $html   =   file_get_contents('index.html');
         $html   =   str_replace('</body>',  $vars. '</body>',  $html);
