ca1d40f7502ccf62a52ea9775ad58ad82c4f359e
pkarasev <p.karasev@psw.ru>
2023-09-26 15:17:45 +0300

    upd

--- php24/lib/res.php
+++ php24/lib/res.php
@@ -2,8 +2,44 @@
 class res
 {
     static $render;
-    static $ret = array();
+    static $ret  =  array();
     
+
+    
+    static function html()
+    {
+        if ( self::$render != 'html' )  return;
+        
+        $vars   =   "";
+        foreach( self::$ret as $k => $v )    $vars  .=  '<div id="' .$k. '">' .json_encode($v). '</div>'. "\n";
+        
+        $html   =   file_get_contents('index.html');
+        $html   =   str_replace('</body>',  $vars. '</body>',  $html);
+
+        die($html);
+    }
+
+
+    static function json()
+    {
+        if ( self::$render != 'json' )  return;
+
+        // ui::vd(req::$wait);
+        // ui::vd(req::$param);
+        // ui::vd(self::$ret);
+
+        header('Content-Type: application/json; charset=utf-8');
+
+        die( json_encode(self::$ret) );
+    }
+
+
+
+
+
+
+
+
 
     static function userList()
     {
@@ -77,7 +113,7 @@ class res
         if ( ! pack::$start )                           return;
         if ( ! in_array(__FUNCTION__, req::$wait) )     return;
 
-        
+
     }
     
     static function lineText()
@@ -115,33 +151,5 @@ class res
 
     
 
-
-
-    static function html()
-    {
-        if ( self::$render != 'html' )  return;
-        
-        $vars   =   "";
-        foreach( self::$ret as $k => $v )    $vars  .=  '<div id="' .$k. '">' .json_encode($v). '</div>'. "\n";
-        
-        $html   =   file_get_contents('index.html');
-        $html   =   str_replace('</body>',  $vars. '</body>',  $html);
-
-        die($html);
-    }
-
-
-    static function json()
-    {
-        if ( self::$render != 'json' )  return;
-
-        // ui::vd(req::$wait);
-        // ui::vd(req::$param);
-        // ui::vd(self::$ret);
-
-        header('Content-Type: application/json; charset=utf-8');
-
-        die( json_encode(self::$ret) );
-    }
 
 }
