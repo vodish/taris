acc3f76cc4239dc3bd4ffdcb154f324acaa33df7
vodish <vodish@yandex.ru>
2023-10-12 21:04:21 +0300

    upd

--- svelte23/src/ui/comp/MenuItem.svelte
+++ svelte23/src/ui/comp/MenuItem.svelte
@@ -12,6 +12,6 @@ $: level1  =   $url.level[1] || "view";
 
 {#if key in $packMenu }
     {#if level1 == key }  <span class="a {key}">{$packMenu[key]}</span>
-    {:else}  <a {href} on:click={pref} class="{key}">{@html $packMenu[key]}</a>
+    {:else}  <a {href} on:click={pref} class="a {key}">{@html $packMenu[key]}</a>
     {/if}
 {/if}
