089a3dbb66c095569c8f333ef830507ae94bc03f
pkarasev <p.karasev@psw.ru>
2023-10-12 14:59:41 +0300

    upd

--- svelte23/src/ui/PackTree.svelte
+++ svelte23/src/ui/PackTree.svelte
@@ -1,34 +1,22 @@
 <script>
-// @ts-nocheck
-import { pref, packStart, treeText } from "../state/store";
-import AceYaml from './comp/AceYaml.svelte';
+    // @ts-nocheck
+    import { pref, packStart, treeText } from "../state/store";
+    import AceYaml from './comp/AceYaml.svelte';
 
 
-
-
-function save()
-{
-    pref(`/${$packStart}`, {tree: $treeText});
-    // api({pack: $packStart, tree: $treeText, wait: ["packStart", "packTitle", "packBc", "packTree"]},
-    //     (res)=>{
-    //         packStart.set(res.packStart)
-    //         packTitle.set(res.packTitle)
-    //         packBc.set(res.packBc)
-    //         packTree.set(res.packTree)
-
-    //         href(`/${$packStart}`)
-    //     }
-    // )
-}
-
-document.onkeydown = (e) => {
-    if ( ['KeyS', 'Enter'].includes(e.code)  &&  (e.ctrlKey || e.metaKey) )
+    function save()
     {
-        e.preventDefault()
-        save()
+        pref(`/${$packStart}`, {tree: $treeText});
     }
-}
 
+
+    document.onkeydown = (e) => {
+        if ( ['KeyS', 'Enter'].includes(e.code)  &&  (e.ctrlKey || e.metaKey) )
+        {
+            e.preventDefault()
+            save()
+        }
+    }
 </script>
 
 {#if $packStart} <AceYaml bind:value={$treeText} /> {/if}
