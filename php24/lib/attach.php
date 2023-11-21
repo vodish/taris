2ce0a41d827e8d9ed236e353325c61b7400e14c1
vodish <vodish@yandex.ru>
2023-11-21 19:10:14 +0300

    upd

--- php24/lib/attach.php
+++ php24/lib/attach.php
@@ -6,56 +6,34 @@ class attach
     #
     static function clipboard()
     {
-        // $file   =   "attach/f3/97/3a/cc/3d/3e/88/a0/8f/5b/62/23/b1/51/18/4f/f3973acc3d3e88a08f5b6223b151184f.png";
-        // die;
-        $dir = $_SERVER['DOCUMENT_ROOT']. '/attach';
-        // chdir($_SERVER['DOCUMENT_ROOT']. '/attach');
-        $image = new Imagick();
-        $image->readImage("$dir/image.png");
-
-        $image->setImageFormat('webp');
-        $image->setImageCompressionQuality(80);
-        $image->setOption('webp:lossless', 'true');
-        $image->writeImage("$dir/image.80.webp");
-
-
-
-        if ( ! user::$id )      return;
-        if ( empty($_FILES) )   return;
+        if ( ! user::$id )                      return;
+        if ( empty($_FILES) )                   return;
         if ( ! isset(req::$param['attach']) )   return;
         if ( ! isset($_FILES['clipboard']) )    return;
         
 
+        //chdir($_SERVER['DOCUMENT_ROOT']);
         # переложить в папку
         #
         $tmp_name   =   $_FILES['clipboard']['tmp_name'];
-        $fname      =   md5(req::$param['rtoken']. $tmp_name);
-        $fdir       =   'attach/'. implode('/', str_split($fname, 2));
+        $name       =   $_FILES['clipboard']['name'];
+        $hash       =   md5(req::$param['rtoken']. $tmp_name);
+        $dir        =   $_SERVER['DOCUMENT_ROOT']. '/attach/'. implode('/', str_split($hash, 2));
         #
-        chdir($_SERVER['DOCUMENT_ROOT']);
-        if ( !file_exists($fdir) )   mkdir($fdir, 0777, true);
+        if ( !file_exists($dir) )   mkdir($dir, 0777, true);
         #
-        #
-        // move_uploaded_file($tmp_name, "$fdir/$fname");
         #
         $image = new Imagick();
         $image->readImage($tmp_name);
-
         $image->setImageFormat('webp');
         $image->setImageCompressionQuality(80);
         $image->setOption('webp:lossless', 'true');
-        
-        chdir($fdir);
-        $image->writeImage($fname);
+        #
+        #
+        $image->writeImage("$dir/$hash");
+        $size       =   filesize("$dir/$hash");
 
-        // $img    =   imagecreatefrompng($tmp_name);
-        // imagepalettetotruecolor($img);
-        // imagealphablending($img, true);
-        // imagesavealpha($img, true);
-        // imagewebp($img, "$fdir/$fname", 80);
-        // imagedestroy($img);
         
-
 
         # добавить запись в базу
         #
@@ -70,9 +48,9 @@ class attach
                 ,`host`
             )
             VALUES (
-                 " .db::v( $fname ). "
-                ," .db::v( $_FILES['clipboard']['name'] ). "
-                ," .db::v( $_FILES['clipboard']['size'] ). "
+                 " .db::v( $hash ). "
+                ," .db::v( $name ). "
+                ," .db::v( $size ). "
                 ," .db::v( author::$email ). "
                 ," .db::v( author::$id ). "
                 ," .db::v( pack::$file ). "
@@ -84,7 +62,7 @@ class attach
 
         # вернуть путь к файлу
         #
-        res::$ret['clipboard']  =   $_SERVER['REQUEST_SCHEME']. "://". HTTP_HOST. "/$fname";
+        res::$ret['clipboard']  =   $_SERVER['REQUEST_SCHEME']. "://". HTTP_HOST. "/$hash";
         
     }
 }
